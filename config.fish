6caaedbc6744686e7849e932a2e0f9da179afdf2
yu-yagishita <yu.yagishita1995@gmail.com>
2020-11-14 16:55:20 +0900

    feat(fish): config.fishの追加

---
+++ config.fish
@@ -0,0 +1,39 @@
+set -U FZF_LEGACY_KEYBINDINGS 0
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+set XDG_CONFIG_HOME ~/.config
+
+# pyenvさんに~/.pyenvではなく、/usr/loca/var/pyenvを使うようにお願いする
+export PYENV_ROOT=/usr/local/var/pyenv
+
+# pyenvさんに自動補完機能を提供してもらう
+#if which pyenv > /dev/null; then eval "(pyenv init -)"; end
+
+# Load pyenv automatically by appending
+# the following to ~/.config/fish/config.fish:
+
+status --is-interactive; and source (pyenv init -|psub)
+
+alias brew="env PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin brew"
+#alias brew="env PATH={PATH/\/usr\/local\/var\/pyenv\/shims/} brew"
+alias g git
+
+function attach_tmux_session_if_needed
+    set ID (tmux list-sessions)
+    if test -z "$ID"
+        tmux new-session
+        return
+    end
+
+    set new_session "Create New Session" 
+    set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+    if test "$ID" = "$new_session"
+        tmux new-session
+    else if test -n "$ID"
+        tmux attach-session -t "$ID"
+    end
+end
+
+if test -z $TMUX && status --is-login
+    attach_tmux_session_if_needed
+end
