198bdd0146be4b18982c96a19ebbd39dd53dbe38
yu-yagishita <yu.yagishita1995@gmail.com>
2021-07-03 02:11:43 +0900

    feat: tmuxを自動復元させた。でiterm起動時のファンクションを無効化

--- config.fish
+++ config.fish
@@ -30,25 +30,25 @@ end
 
 eval (gh completion -s fish| source)
 
-function attach_tmux_session_if_needed
-    set ID (tmux list-sessions)
-    if test -z "$ID"
-        tmux new-session
-        return
-    end
-
-    set new_session "Create New Session"
-    set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
-    if test "$ID" = "$new_session"
-        tmux new-session
-    else if test -n "$ID"
-        tmux attach-session -t "$ID"
-    end
-end
-
-if test -z $TMUX && status --is-login
-    attach_tmux_session_if_needed
-end
+# function attach_tmux_session_if_needed
+#     set ID (tmux list-sessions)
+#     if test -z "$ID"
+#         tmux new-session
+#         return
+#     end
+# 
+#     set new_session "Create New Session"
+#     set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+#     if test "$ID" = "$new_session"
+#         tmux new-session
+#     else if test -n "$ID"
+#         tmux attach-session -t "$ID"
+#     end
+# end
+# 
+# if test -z $TMUX && status --is-login
+#     attach_tmux_session_if_needed
+# end
 
 # githubにリポジトリを作り、ghqで取得、vscodeでひらく
 function ghcr
