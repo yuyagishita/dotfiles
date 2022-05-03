6cee798b0f5bf3c476ca5cbc3a64597acd6be9cf
yuyagishita <yu.yagishita1995@gmail.com>
2022-05-03 13:04:21 +0900

    feat: init.lua導入した！！！！

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,4 @@
-if not vim.fn.exists('g:vscode') then
-  -- execute 'source ~/.vimrc
-
-  -- true color
-  if vim.fn.exists("&termguicolors") && vim.fn.exists("&winblend") then
-    let g:neosolarized_termtrans=1
-    runtime ./colors/solarized_true.vim
-    set termguicolors
-    set winblend=0
-    set wildoptions=pum
-    set pumblend=5
-  end
-
-end
+require('options')
+require('display')
+require('plugins')
+require('mappings')
