7fe7bc77aa780be86c6745d88598d73b576d109a
yuyagishita <yu.yagishita1995@gmail.com>
2023-04-19 13:25:07 +0900

    fix(lspconfig): setup()を使ってlspを起動するようにした
    setup_handlers()がhaskell以外動かなかったため。おそらくパスとかそこらへんがうまくいってなさそうではある。

--- .config/nvim/lua/pluginconfig/mason-lspconfig.lua
+++ .config/nvim/lua/pluginconfig/mason-lspconfig.lua
@@ -2,6 +2,8 @@ require("mason-lspconfig").setup()
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
+-- selene: allow(unused_variable)
+---@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
 	local function buf_set_keymap(...)
 		vim.api.nvim_buf_set_keymap(bufnr, ...)
@@ -45,48 +47,62 @@ local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- setup_handlers内にいれても正常に動作しないので、手動実行
+-- setup_handlersを使うとhls以外「0 client(s) attached to this buffer」になるケースがほとんど
 require("clangd_extensions").setup({ server = { capabilities = capabilities, on_attach = on_attach } })
+require("rust-tools").setup({ server = { capabilities = capabilities, on_attach = on_attach } })
+lspconfig.lua_ls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+ 		  diagnostics = {
+ 			  globals = { "vim" },
+ 		  },
+ 	  },
+  },
+})
 
 require("mason-lspconfig").setup_handlers({
 	function(server_name)
 		lspconfig[server_name].setup({ capabilities = capabilities, on_attach = on_attach })
 	end,
-  ["clangd"] = function()
-    local has_clangd_extensions, clangd_extensions = pcall(require, "clangd_extensions")
-    if has_clangd_extensions then
-      capabilities.offsetEncoding = { "utf-16" }
-      clangd_extensions.setup({ server = { capabilities = capabilities, on_attach = on_attach } })
-    else
-      lspconfig.clangd.setup({ server = { capabilities = capabilities, on_attach = on_attach } })
-    end
-  end,
+  -- ["clangd"] = function()
+  --   local has_clangd_extensions, clangd_extensions = pcall(require, "clangd_extensions")
+  --   if has_clangd_extensions then
+  --     capabilities.offsetEncoding = { "utf-16" }
+  --     clangd_extensions.setup({ server = { capabilities = capabilities, on_attach = on_attach } })
+  --   else
+  --     lspconfig.clangd.setup({ capabilities = capabilities, on_attach = on_attach })
+  --   end
+  -- end,
+  -- haskellだけsetup_handlers内に入れても動く。
   ["hls"] = function()
     local has_haskell_tools, haskell_tools = pcall(require, "haskell-tools")
     if has_haskell_tools then
       haskell_tools.setup({ server = { capabilities = capabilities, on_attach = on_attach } })
     else
-      lspconfig.hls.setup({ server = { capabilities = capabilities, on_attach = on_attach } })
+      lspconfig.hls.setup({ { capabilities = capabilities, on_attach = on_attach } })
     end
   end,
-	["rust_analyzer"] = function()
-		local has_rust_tools, rust_tools = pcall(require, "rust-tools")
-		if has_rust_tools then
-			rust_tools.setup({ server = { capabilities = capabilities, on_attach = on_attach } })
-		else
-			lspconfig.rust_analyzer.setup({})
-		end
-	end,
-	["lua_ls"] = function()
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-		  settings = {
-        Lua = {
-		 		  diagnostics = {
-		 			  globals = { "vim" },
-		 		  },
-		 	  },
-		  },
-    })
-		end,
+	-- ["rust_analyzer"] = function()
+	-- 	local has_rust_tools, rust_tools = pcall(require, "rust-tools")
+	-- 	if has_rust_tools then
+	-- 		rust_tools.setup({ server = { capabilities = capabilities, on_attach = on_attach } })
+	-- 	else
+	-- 		lspconfig.rust_analyzer.setup({ capabilities = capabilities, on_attach = on_attach })
+	-- 	end
+	-- end,
+	-- ["lua_ls"] = function()
+ --    lspconfig.lua_ls.setup({
+ --      capabilities = capabilities,
+ --      on_attach = on_attach,
+	-- 	  settings = {
+ --        Lua = {
+	-- 	 		  diagnostics = {
+	-- 	 			  globals = { "vim" },
+	-- 	 		  },
+	-- 	 	  },
+	-- 	  },
+ --    })
+	-- end,
 })
