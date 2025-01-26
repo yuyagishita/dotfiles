fce57af04772b578cd67f3fd65fb44f9836bfe34
yuyagishita <yu.yagishita1995@gmail.com>
2025-01-27 01:32:09 +0900

    ok

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
+    event = "VimEnter",
     config =  function()
       local function is_available_gps()
       	local ok, _ = pcall(require, "nvim-gps")
@@ -172,6 +172,25 @@ return {
       	extensions = { "quickfix", my_toggleterm, "symbols-outline", my_extension },
       })
     end,
-  },
+    dependencies = {
+      {
+        "EdenEast/nightfox.nvim",
+     		config = function()
+          local nightfox = require("nightfox")
+          nightfox.setup({
+          	options = {
+          		dim_inactive = true, -- Non focused panes set to alternative background
+          	},
+          })
+          vim.cmd.colorscheme("nordfox")
+          local compile_path = vim.fn.stdpath("cache") .. "/nightfox"
+          local stats = vim.uv.fs_stat(compile_path)
+          local is_directory = (stats and stats.type == "directory") or false
+          if is_directory then
+          	nightfox.compile()
+          end
+     		end,
+      }
+    },
+  }
 }
-
