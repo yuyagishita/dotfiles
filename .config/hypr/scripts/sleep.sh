555bf23ed0ae7f92382902c74f5be0efd9c83222
yuyagishita <yu.yagishita1995@gmail.com>
2025-02-19 18:12:46 +0900

    OK

--- .config/hypr/scripts/sleep.sh
+++ .config/hypr/scripts/sleep.sh
@@ -1,3 +1,3 @@
 swayidle -w timeout 300 'swaylock -f -c 000000' \
             timeout 600 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000' &
+            before-sleep 'swaylock -f -c 000000'
